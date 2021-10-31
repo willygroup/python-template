dd2cd0778de6c9720471d0e24629c8f9c7427b68
Daniele Forti <d.forti@affidaty.io>
2021-10-31 11:14:30 +0100

    added function to allow only `sourcing` of linux script

--- create_python_venv_linux.sh
+++ create_python_venv_linux.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
-python -m venv .linux_env && . .linux_env/bin/activate && pip install -r requirements.txt
+if [ ${0##*/} == ${BASH_SOURCE[0]##*/} ]; then 
+    echo "WARNING"
+    echo "This script is not meant to be executed directly!"
+    echo "Use this script only by sourcing it."
+    echo
+    exit 1
+fi
+python -m venv .linux_env && . ./.linux_env/bin/activate && pip install -r requirements.txt
--- linux_env.sh
+++ linux_env.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
+if [ ${0##*/} == ${BASH_SOURCE[0]##*/} ]; then 
+    echo "WARNING"
+    echo "This script is not meant to be executed directly!"
+    echo "Use this script only by sourcing it."
+    echo
+    exit 1
+fi
 source ./.linux_env/bin/activate
